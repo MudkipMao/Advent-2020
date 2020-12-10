46f819640c817f988e21bd22a2efecae6aebd4d7
MudkipMao <splatt9198@gmail.com>
2020-12-09 17:35:52 -0800

    Finished day 4 part 2

--- day-4/src/main.rs
+++ day-4/src/main.rs
@@ -47,29 +47,119 @@ fn part1(filename: String) {
 }
 
 fn part2(filename: String) {
-    let args: Vec<String> = env::args().collect();
-    let num_down = &args[3].parse::<usize>().unwrap();
-    let num_right = &args[4].parse::<usize>().unwrap();
-    let num_down: usize = num_down.clone();
-    let num_right: usize = num_right.clone();
     let contents = fs::read_to_string(filename)
         .expect("Something went wrong reading the file");
-    let split: Vec<&str> = contents.split("\n").collect();
-    let mut row_index = 0;
-    let mut col_index = 0;
-    let mut tree_count = 0;
-    let line_len = split[0].len();
-
-    for line in &split {
-        if (row_index % num_down) != 0 {
-            row_index += 1;
+    let file_str: Vec<&str> = contents.split("\n\n").collect();
+    let mut num_valid_passport = 0;
+    // Iterate over passports in file
+    for passport in &file_str {
+        let passport_split: Vec<&str> = passport.split_whitespace().collect();
+        // I will assume that there cannot be any incorrect fields for now
+        if passport_split.len() < 7 {
             continue;
         }
-        if line.chars().nth(col_index).unwrap() == '#'{
-            tree_count += 1;
+        // Iterate over fields in passport
+        let mut cid_found: bool = false;
+        let mut fields_valid: bool = true;
+        for field in &passport_split {
+            if !fields_valid {
+                break;
+            }
+            let key_value: Vec<&str> = field.split(":").collect();
+            match key_value[0] {
+               "byr" => {
+                   let age = key_value[1].parse::<usize>().unwrap();
+                   if !(age >= 1920 && age <= 2002) {
+                       fields_valid = false;
+                       break;
+                   }
+               }
+               "iyr" => {
+                   let year = key_value[1].parse::<usize>().unwrap();
+                   if !(year >= 2010 && year <= 2020) {
+                       fields_valid = false;
+                       break;
+                   }
+               }
+               "eyr" => {
+                   let year = key_value[1].parse::<usize>().unwrap();
+                   if !(year >= 2020 && year <= 2030) {
+                       fields_valid = false;
+                       break;
+                   }
+               }
+               "hgt" => {
+                   let len_value = key_value[1].len();
+                   let units = key_value[1].chars().nth(len_value - 1).unwrap();
+                   let mut metric = false;
+                   if units == 'm' {
+                       metric = true;
+                   }
+                   else if units != 'n' { // This means that we're neither cm nor in 
+                       fields_valid = false;
+                       break;
+                   }
+                   let mut numeric_height: u32 = 0;
+                   for c in key_value[1].chars() {
+                       if !c.is_numeric() {
+                           break;
+                       }
+                       numeric_height = (10 * numeric_height) + c.to_digit(10).unwrap();
+                   }
+                   if metric {
+                       if !(numeric_height >= 150 && numeric_height  <= 193) {
+                           fields_valid = false;
+                       } 
+                    } else {
+                       if !(numeric_height >= 59 && numeric_height  <= 76) {
+                           fields_valid = false;
+                   }
+                }
+                   
+               }
+               "hcl" => {
+                   if key_value[1].len() != 7 {
+                       fields_valid = false;
+                       break;
+                   }
+                   if key_value[1].chars().nth(0).unwrap() != '#' {
+                       fields_valid = false;
+                       break;
+                   }
+                   let mut sliced = "";
+                   unsafe {
+                       sliced = key_value[1].get_unchecked(1..key_value[1].len())
+                   }
+                   match i64::from_str_radix(sliced, 16) {
+                       Ok(_num) => (),
+                       Err(_e) => fields_valid = false,
+                   }
+               }
+               "ecl" => {
+                   match key_value[1] {
+                       "amb" | "blu" | "brn" | "gry" | "grn" | "hzl" | "oth" => (),
+                       _ => fields_valid = false,
+                   }
+                
+               }
+               "pid" => {
+                   if (key_value[1].len()) != 9 || !key_value[1].parse::<u32>().is_ok() {
+                       fields_valid = false;
+                   }
+               }
+               "cid" => {
+                   cid_found = true;
+               }
+               _ => {
+                   println!("Unknown key {}!", key_value[0]);
+               }
+            }
         }
-        col_index = (col_index + num_right) % line_len;
-        row_index += 1;
+        if (passport_split.len() == 8) && fields_valid {
+            num_valid_passport += 1;
+        } else if (passport_split.len() == 7) && fields_valid && !cid_found {
+            num_valid_passport += 1;
+        }
     }
-    println!("Number of trees is {}, Line len is {}", tree_count, line_len);
+    println!("Number of valid passports is {} out of {}", num_valid_passport, file_str.len());
 }
