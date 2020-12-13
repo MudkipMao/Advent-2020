a7021f7ab93623db835665a828b0571c795e83aa
MudkipMao <splatt9198@gmail.com>
2020-12-12 20:06:48 -0800

    Finished day 6 part 2

--- day-6/src/main.rs
+++ day-6/src/main.rs
@@ -1,6 +1,7 @@
 use std::env;
 use std::fs;
 use std::collections::HashSet;
+use std::collections::HashMap;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -38,20 +39,36 @@ fn part1(filename: String) {
 fn part2(filename: String) {
     let contents = fs::read_to_string(filename)
         .expect("Something went wrong reading the file");
-    let file_iter = contents.split("\n\n");
+    let file_iter: Vec<&str> = contents.split("\n\n").collect();
     let mut total_count = 0;
     // Iterate over groups per file
     for group in file_iter {
-        let person_iter = group.split("\n");
-        let mut seen_values: HashSet<char> = HashSet::new();
+        let person_iter: Vec<&str> = group.split("\n").collect();
+        let mut seen_values: HashMap<char, usize> = HashMap::new();
+        let group_size = person_iter.len();
         // Iterate over person per group
         for person in person_iter {
             // Iterate over answers per person
             for c in person.chars() {
-                seen_values.insert(c);
+                // print!("person is {}, groupsize is {}, curr char is {}", person, group_size, c);
+                match seen_values.get(&c) {
+                    Some(answer) => {
+                        let new_val = answer + 1;
+                        seen_values.insert(c, new_val);
+                        if new_val == group_size {
+                            total_count += 1;
+                        }
+                    }
+                    None => {
+                        seen_values.insert(c, 1);
+                        if group_size == 1 {
+                            total_count += 1;
+                        }
+                    }
+                }
+                // println!(" count is {}", seen_values.get(&c).unwrap());
             }
         }
-        total_count += seen_values.len();
     }
     println!("Total count is {}", total_count);
 }
